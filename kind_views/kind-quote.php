bba900dad5a162c5b68a91800d906685e83ca383
Neil Mather <ngmather@gmail.com>
2017-04-16 14:31:51 +0100

    Tidy up kind-quote template

--- kind_views/kind-quote.php
+++ kind_views/kind-quote.php
@@ -5,23 +5,25 @@
 
 $kind = get_post_kind_slug( get_the_ID() );
 $meta = new Kind_Meta( get_the_ID() );
-$context['author'] = Kind_View::get_hcard( $meta->get_author() );
+
 $cite = $meta->get_cite();
-$context['site_name'] = Kind_View::get_site_name( $meta->get_cite(), $meta->get_url() );
-$context['title'] = Kind_View::get_cite_title( $meta->get_cite(), $meta->get_url() );
-$embed = self::get_embed( $meta->get_url() );
-$context['embed'] = $embed;
-$context['meta_url'] = $meta->get_url();
+$url = $meta->get_url();
+$embed = self::get_embed($url);
+
+$context['quotation_url'] = $url;
+$context['title'] = Kind_View::get_cite_title( $cite, $url );
+$context['author'] = Kind_View::get_hcard( $meta->get_author() );
 $context['by'] = __( 'by', 'indieweb-post-kinds' );
+$context['site_name'] = Kind_View::get_site_name( $cite, $url );
+
 $context['cite'] = $cite;
+$context['embed'] = $embed;
 
 if ($embed) {
-    $context['quoteText'] = $embed;
+    $context['showEmbed'] = true;
 } else if ( array_key_exists( 'summary', $cite ) ) {
-    $context['quoteText'] = $cite['summary'];
+    $context['showCiteSummary'] = true;
 }
 
 Timber::render('kind-quote.twig', $context);
-
 ?>
-
