fd6c5d1ce9a420a50a27ff6720dc32e147311b73
Neil Mather <ngmather@gmail.com>
2018-09-20 20:17:21 +0100

    Add rel-me for Mastodon

--- header.php
+++ header.php
@@ -18,7 +18,8 @@
 <link href="https://fonts.googleapis.com/css?family=Monofett" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css?family=Fredoka+One" rel="stylesheet">
-<?php wp_head(); ?>
+?php wp_head(); ?>
+<link rel="me" href="https://social.coop/@neil" />
 </head>
 
 <body <?php body_class(); ?><?php sempress_semantics( 'body' ); ?>>
